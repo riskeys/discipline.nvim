eb70b3900ddbaed3474d72a71c8491d7a9b4f85a
R. Rakhman <riskeys32@gmail.com>
2026-09-08 21:24:35 +0700

    rename local/remote module

--- lua/discipline/main.lua
+++ lua/discipline/main.lua
@@ -1,5 +1,5 @@
-local dc_scratch = require("discipline.scratch")
-local local_scratch = require("discipline.local_scratch")
+local remote = require("discipline.remote")
+local locald = require("discipline.local")
 local wnd = require("discipline.window")
 local praytell = require("discipline.praytell")
 
@@ -18,33 +18,33 @@ end
 
 local M = {}
 
-M.set_repo_notes_path = local_scratch.set_notes_path
-M.set_local_scratch_subdir = local_scratch.set_local_scratch_subdir
-M.set_local_reminder_subdir = local_scratch.set_local_reminder_subdir
-M.set_local_daily_subdir = local_scratch.set_local_daily_subdir
+M.set_repo_notes_path = locald.set_notes_path
+M.set_local_scratch_subdir = locald.set_local_scratch_subdir
+M.set_local_reminder_subdir = locald.set_local_reminder_subdir
+M.set_local_daily_subdir = locald.set_local_daily_subdir
 
 M.open_reminder_top = function()
-	local buf_reminder = dc_scratch.get_reminder_buf()
+	local buf_reminder = remote.get_reminder_buf()
 	toggle_top(buf_reminder)
 end
 
 M.open_reminder_btm = function()
-	local buf_reminder = dc_scratch.get_reminder_buf()
+	local buf_reminder = remote.get_reminder_buf()
 	toggle_btm(buf_reminder)
 end
 
 M.open_daily_btm = function()
-	local buf_daily = dc_scratch.get_scratch_daily_buf()
+	local buf_daily = remote.get_scratch_daily_buf()
 	toggle_btm(buf_daily)
 end
 
 M.open_daily_local_btm = function()
-	local buf_daily = local_scratch.get_scratch_daily_buf()
+	local buf_daily = locald.get_scratch_daily_buf()
 	toggle_top(buf_daily)
 end
 
 M.open_daily_local_top = function()
-	local buf_daily = local_scratch.get_scratch_daily_buf()
+	local buf_daily = locald.get_scratch_daily_buf()
 	toggle_top(buf_daily)
 end
 
@@ -54,17 +54,17 @@ M.open_random_buffer_test = function()
 end
 
 M.open_scratch_btm = function()
-	local buf = dc_scratch.get_scratch_buf()
+	local buf = remote.get_scratch_buf()
 	toggle_btm(buf)
 end
 
 M.open_scratch_top = function()
-	local buf = dc_scratch.get_scratch_buf()
+	local buf = remote.get_scratch_buf()
 	toggle_top(buf)
 end
 
 M.open_scratch_daily = function()
-	local buf = dc_scratch.get_scratch_daily_buf()
+	local buf = remote.get_scratch_daily_buf()
 	toggle_top(buf)
 end
 
@@ -74,17 +74,17 @@ M.open_praytell = function()
 end
 
 M.open_local_reminder = function()
-	local buf_reminder = local_scratch.get_reminder_buf()
+	local buf_reminder = locald.get_reminder_buf()
 	toggle_btm(buf_reminder)
 end
 
 M.open_local_scratch_top = function()
-	local buf_scratch = local_scratch.get_scratch_buf()
+	local buf_scratch = locald.get_scratch_buf()
 	toggle_top(buf_scratch)
 end
 
 M.open_current_repo_notes = function()
-	local buf = local_scratch.get_notes_buf()
+	local buf = locald.get_notes_buf()
 	toggle_top(buf)
 end
 
@@ -92,7 +92,7 @@ M.wnd_toggle_float_btm = wnd.toggle_float_btm
 M.wnd_toggle_float_top = wnd.toggle_float_top
 M.wnd_close_window = wnd.close_window
 M.wnd_close_window_top = wnd.close_window_top
-M.scratch_open_git = dc_scratch.open_git
+M.scratch_open_git = remote.open_git
 M.wnd_switch_window = wnd.switch_window
 
 M.arg_handler = function(opts)
@@ -142,7 +142,7 @@ M.arg_handler = function(opts)
 	end
 
 	if opts.args == "git" then
-		dc_scratch.open_git()
+		remote.open_git()
 		return
 	end
 
@@ -152,29 +152,6 @@ M.arg_handler = function(opts)
 	end
 
 	vim.notify("Command unknown!", vim.log.levels.WARN)
-end
-
-
-M.discipline_complete = function(arg_lead, cmd_line, cursor_pos)
-	local commands = {
-		"git",
-		"local_reminder",
-		"local_scratch",
-		"local_scratch_daily",
-		"scratch_daily",
-		"scratch",
-		"scratch_btm",
-		"reminder",
-		"praytell",
-		"repo_notes",
-	}
-	local matches = {}
-	for _, cmd in ipairs(commands) do
-		if vim.startswith(cmd, arg_lead) then
-			table.insert(matches, cmd)
-		end
-	end
-	return matches
 end
 
 return M
